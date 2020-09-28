b39d584493c8f31d1043fabc8b31e2cb4827ca7b
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-28 09:14:43 -0400

    Sessions now working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'products/index'
+  # get 'products/index'
 
-  get 'products/add'
+  # get 'products/add'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index'
+  post '/' => 'products#add'
+
 end
